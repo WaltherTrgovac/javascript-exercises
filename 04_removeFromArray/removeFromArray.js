8e5ed579d1be6ee973c8522e90140de06ab6d0d5
Walther <razi.black2014@hotmail.com>
2025-04-20 00:44:21 +0200

    add 4-7 exercise solutions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(list) {
+    result = [];
+    for (let i = 0; i < list.length; i++){
+        inArray = false;
+        for (let j = 1; j < arguments.length; j++){
+            if (list[i] === arguments[j]){
+                inArray = true;
+            }
+        }
+        if (!inArray){
+            result.push(list[i]);
+        }
+    }
+
+    return result 
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
 const sumAll = function() {
+    if (!Number.isInteger(arguments[0]) || !Number.isInteger(arguments[1])) return 'ERROR';
+    if (arguments[0] < 0 || arguments[1] < 0) return 'ERROR';
 
+    let result = 0;
+    for (let i = Math.min(arguments[0], arguments[1]); i <= Math.max(arguments[0], arguments[1]); i++){
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
